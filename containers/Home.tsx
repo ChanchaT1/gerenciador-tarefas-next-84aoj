d5d46f6ac2a3daa89936205de17fcd7dcc317048
Gustavo Chancharulo <gustavochancha29@gmail.com>
2021-12-02 22:15:00 -0300

    Aula 4 - Parte 2

--- containers/Home.tsx
+++ containers/Home.tsx
@@ -1,3 +1,21 @@
-export const Home = () => {
-    return (<h1>Bem vindo</h1>);
+import { NextPage } from "next";
+import { Header } from "../components/Header";
+
+type HomeProps = {
+    setToken(s: string) : void
+}
+
+export const Home : NextPage<HomeProps> = ({setToken}) => {
+
+    const sair = () =>{
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('userName');
+        localStorage.removeItem('userEmail');
+        setToken('');
+    }
+
+    return (
+    <>
+        <Header sair={sair}/>
+    </>);
 }
